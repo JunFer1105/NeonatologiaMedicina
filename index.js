bbf3eb04bae257b010e5d3210c7a3c23568ea098
Juan Fernando Arango <junfer.1105@gmail.com>
2020-04-05 12:35:46 -0500

    Primera Version

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/',function(req,res){
 	res.send('Neonatologia');
 })
 
-app.listen(app.get('port'), ()=> {
-	console.log('Escuchando en el puerto ',app.get('port'));
+app.listen(3000, ()=> {
+	console.log('Escuchando en el puerto 3000');
 });
